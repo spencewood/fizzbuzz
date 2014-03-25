bd517ec405fec67a8545cc05a5caddc81303f507
Tyler Spencewood <tspencewood@gmail.com>
2014-03-25 13:45:45 +0700

    Adding description readme. Adding JS module.

---
+++ javascript/fizzbuzz.js
@@ -0,0 +1,18 @@
+var fizzbuzz = function(min, max){
+  var fb = [], arr = [];
+  min = min == void null ? 1 : min;
+  max = max == void null ? 100 : max;
+  for(; min <= max; min++){
+    fb = [];
+    if(min % 3 === 0){
+      fb.push('Fizz');
+    }
+    if(min % 5 === 0){
+      fb.push('Buzz');
+    }
+    arr.push(fb.length === 0 ? min : fb.join(' '));
+  }
+  return arr;
+};
+
+module.exports = fizzbuzz;
