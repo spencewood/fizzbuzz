2ec0529fdcf07680362fe1567fa4478fd41d7531
Tyler Spencewood <tspencewood@gmail.com>
2014-03-27 09:37:10 +0700

    Adding vb.

---
+++ vb4.5/FizzBuzz/FizzBuzz.vb
@@ -0,0 +1,34 @@
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Linq
+Imports System.Collections.Generic
+
+Public Class FizzBuzz
+	Private _min as Integer
+	Private _max as Integer
+	
+	Public Sub New(Optional ByVal min As Integer = 1, Optional ByVal max As Integer = 100)
+		MyBase.New
+		_min = min
+		_max = max
+	End Sub
+	
+	Public Function Process() As IEnumerable(Of String)
+		'Dim x = Enumerable.Range(_min, _max).Select(Function(x) x)
+	End Function
+	
+	Private Function Say(ByVal x As Integer) As String
+		If x mod 15 = 0 Then
+			return "FizzBuzz"
+		ElseIf x mod 3 = 0 Then
+			return "Fizz"
+		ElseIf x mod 5 = 0 Then
+			return "Buzz"
+		Else
+			return x.ToString()
+		End If
+	End Function
+End Class
+
