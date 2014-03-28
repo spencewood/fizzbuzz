168bfcbaa176bc19caa7851cfd038ecc0093dce2
Tyler Spencewood <tspencewood@gmail.com>
2014-03-28 21:46:54 +0700

    Adding main python.

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -3,15 +3,15 @@ class FizzBuzz:
     self.min = min
     self.max = max
 
-  def process(self):
+  def generate(self):
     return [self.say(x) for x in range(self.min, self.max + 1)]
 
   def say(self, x):
     if x % 15 == 0:
-      return "Fizz Buzz"
+      return "FizzBuzz"
     elif x % 3 == 0:
       return "Fizz"
     elif x % 5 == 0:
       return "Buzz"
     else:
-      return x
+      return str(x)
---
+++ python/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from fizzbuzz import FizzBuzz
+
+fb = FizzBuzz()
+print ",".join(fb.generate())
