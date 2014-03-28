ff901fa2678e9b1bb931a36d931db63e1a7de918
Tyler Spencewood <tspencewood@gmail.com>
2014-03-28 23:15:57 +0700

    Fixing comile error.

--- haskell/FizzBuzz.hs
+++ haskell/FizzBuzz.hs
@@ -2,7 +2,7 @@ module FizzBuzz where
 import Data.Maybe
 
 generate :: Int -> Int -> [String]  
-generate n m = [ say fb | fb <- [n'..m'], n' <= m' ] -- needs default parameters
+generate n m = [ say fb | fb <- [n..m], n <= m ] where -- needs default parameters
     say x
       | x `mod` 15 == 0 = "FizzBuzz"
       | x `mod` 3 == 0 = "Fizz"
