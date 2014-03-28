e543c88730d52c653b93e83d00f4fcba933094c9
Tyler Spencewood <tspencewood@gmail.com>
2014-03-28 20:10:35 +0700

    Removing lowercase version.

--- haskell/fizzbuzz.hs
+++
@@ -1,7 +0,0 @@
-fizzBuzz :: Int -> Int -> [String]
-fizzBuzz n m = [ say fb | fb <- [n..m], n <= m ] where -- needs default values
-  say x
-    | x `mod` 15 == 0 = "Fizz Buzz"
-    | x `mod` 3 == 0 = "Fizz"
-    | x `mod` 5 == 0 = "Buzz"
-    | otherwise = show x
