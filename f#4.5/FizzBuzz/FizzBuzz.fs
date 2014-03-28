5ecee2e466e4b922a68dd2ec81046a0504c7980f
Tyler Spencewood <tspencewood@gmail.com>
2014-03-29 00:07:17 +0700

    Adding F#.

---
+++ f#4.5/FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,10 @@
+﻿module FizzBuzz.FizzBuzz
+
+let generate n m = 
+    let say x =
+        match x with
+        | f when f % 15 = 0 -> "FizzBuzz"
+        | f when f % 3 = 0 -> "Fizz"
+        | f when f % 5 = 0 -> "Buzz"
+        | _ -> x.ToString()
+    [ for x in n..m -> say x ]
---
+++ f#4.5/FizzBuzz/Program.fs
@@ -0,0 +1,6 @@
+﻿module FizzBuzz.Main
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.WriteLine (FizzBuzz.FizzBuzz.generate 1 100 |> String.concat ",")
+    0
