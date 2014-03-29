b2110ee137313f9134e84b3cf54f97d306403a6f
Tyler Spencewood <tspencewood@gmail.com>
2014-03-30 01:12:14 +0700

    Quick cleanup.

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,15 +1,18 @@
 #include "fizzbuzz.h"
 
-int main()
-{
+string comp(){
   FizzBuzz *f = new FizzBuzz(1, 100);
   vector<string> r = f->generate();
   stringstream ss;
-  for(size_t i = 0; i < r.size(); ++i)
-  {
+  for(size_t i = 0; i < r.size(); ++i){
     if(i != 0)
       ss << ",";
     ss << r[i];
   }
-  cout << ss.str() << "\n";
+  return ss.str();
+}
+
+int main()
+{
+  cout << comp() << "\n";
 }
